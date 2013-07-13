917020559c93d626686dc2c0b3f6c49eb1f6a191
Bart Riemens <briemens@crafity.com>
2013-07-13 03:17:22 +0200

    Updated all the tests

--- test/crafity.filesystem.test.js
+++ test/crafity.filesystem.test.js
@@ -1,67 +1,55 @@
+/*jslint node: true, bitwise: true, unparam: true, maxerr: 50, white: true, stupid: true */
+"use strict";
+
 /*!
- * crafity.filesystem.test - Filesystem tests
- * Copyright(c) 2011 Crafity
- * Copyright(c) 2011 Bart Riemens
- * Copyright(c) 2011 Galina Slavova
+ * crafity-core - Crafity core helper library
+ * Copyright(c) 2013 Crafity
+ * Copyright(c) 2013 Bart Riemens
+ * Copyright(c) 2013 Galina Slavova
  * MIT Licensed
  */
 
 /**
  * Test dependencies.
  */
-var jstest = require('crafity-jstest')
-	, assert = jstest.assert
-	, context = jstest.createContext()
-	, fs = require('../main.js');
 
-// Print out the name of the test module
-console.log("Testing 'main.js' in crafity-filesystem... ");
+var jstest = require('crafity-jstest')
+  , assert = jstest.assert
+  , context = jstest.createContext("Testing crafity filesystem")
+  , fs = require('../main.js');
 
 /**
  * The tests
  */
 var tests = {
 
-	'crafity.filesystem must be the same as the default fs module': function () {
+  'crafity.filesystem must be the same as the default fs module': function () {
 
-		assert.isDefined(fs, "Expected fs to be defined");
-		assert.areEqual(require('fs'), fs.__proto__, "Expected fs to be the standard module");
-	},
+    assert.isDefined(fs, "Expected fs to be defined");
+    
+    var proto = '__proto__';
+    
+    assert.areEqual(require('fs'), fs[proto], "Expected fs to be the standard module");
+  },
 
-	'Combine must be able to concat paths and filenames correctly': function () {
+  'Combine must be able to concat paths and filenames correctly': function () {
 
-		assert.areEqual("foo/bar", fs.combine("foo", "bar"), '"foo", "bar"');
-		assert.areEqual("foo/bar", fs.combine("foo/", "bar"), '"foo/", "bar"');
-		assert.areEqual("foo/bar", fs.combine("foo", "/bar"), '"foo", "/bar"');
-		assert.areEqual("foo/bar", fs.combine("foo/", "/bar"), '"foo/", "/bar"');
-		assert.areEqual("/", fs.combine("/"), '"/"');
-		assert.areEqual("/", fs.combine("/", "/"), '"/", "/"');
-		assert.areEqual("/foo/bar", fs.combine("/", "foo/", "/bar"), '"/", "foo/", "/bar"');
-		assert.areEqual("/foo/bar", fs.combine("/", "/foo/", "/bar"), '"/", "/foo/", "/bar"');
-	},
+    assert.areEqual("foo/bar", fs.combine("foo", "bar"), '"foo", "bar"');
+    assert.areEqual("foo/bar", fs.combine("foo/", "bar"), '"foo/", "bar"');
+    assert.areEqual("foo/bar", fs.combine("foo", "/bar"), '"foo", "/bar"');
+    assert.areEqual("foo/bar", fs.combine("foo/", "/bar"), '"foo/", "/bar"');
+    assert.areEqual("/", fs.combine("/"), '"/"');
+    assert.areEqual("/", fs.combine("/", "/"), '"/", "/"');
+    assert.areEqual("/foo/bar", fs.combine("/", "foo/", "/bar"), '"/", "foo/", "/bar"');
+    assert.areEqual("/foo/bar", fs.combine("/", "/foo/", "/bar"), '"/", "/foo/", "/bar"');
+  },
 
-	'crafity.fylesystem must be the fullname of this module': function () {
-		
-		assert.areEqual("crafity.filesystem", fs.fullname(), "Expected module name is crafity.filesystem!");
-	},
+  'crafity.fylesystem must have package.json file': function () {
 
-	'crafity.fylesystem must have package.json file': function () {
-
-		fs.readFile("./package.json", function (err, data) {
-			assert.isDefined(data, "Expected package.json defined");
-		});
-	},
-
-	'crafity.fylesystem must have the same version as quoted in package.json': function () {
-
-		fs.readFile("./package.json", function (err, data) {
-			var json = JSON.parse(data.toString());
-//			console.log("json", json);
-
-			assert.isDefined(json.version, "Expected fs to be defined");
-			assert.areEqual(fs.version(), json.version, "Expected the same module version!");
-		});
-	}
+    fs.readFile("./package.json", function (err, data) {
+      assert.isDefined(data, "Expected package.json defined");
+    });
+  }
 
 };
 
